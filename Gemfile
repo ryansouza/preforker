b89551ad573dd4e287c91e3f12cbc66ceff4f406
Ryan Souza <ryan.souza@legitscript.com>
2016-01-08 15:27:14 -0800

    Bump rspec to 3.X

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "http://rubygems.org"
 
 gem 'rake'
-gem 'rspec', "~> 2.8"
+gem 'rspec', "~> 3.4"
 gem 'file_test_helper', "~> 1.0"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,9 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
 require 'preforker'
 require 'rspec'
-require 'rspec/autorun'
 require 'rubygems'
-
-RSpec.configure do |c|
-  c.treat_symbols_as_metadata_keys_with_true_values = true
-end
 
 Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f}
 
